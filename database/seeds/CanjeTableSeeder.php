d37fae7bd77443c8c3bb6eba7cd4372ca8e29556
christianvargasb.cr <christianvargasb.cr@gmail.com>
2018-08-18 18:16:28 -0600

    created required files and empty views

---
+++ database/seeds/CanjeTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CanjeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
